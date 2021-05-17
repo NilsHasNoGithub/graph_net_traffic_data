66455738452ad7659eaf1f0446810a5df0aca234
Nils Golembiewski <nilsgolembiewski@gmail.com>
2021-05-17 13:59:52 +0200

    initial commit

--- agents/max_pressure_agent.py
+++ agents/max_pressure_agent.py
@@ -1,23 +1,9 @@
-from abc import abstractmethod, ABC
-
-from roadnet_graph import Intersection
 import random
+
 import numpy as np
 
-
-class Agent(ABC):
-
-    @abstractmethod
-    def get_intersection(self) -> Intersection:
-        raise NotImplementedError()
-
-    @abstractmethod
-    def act(self, engine, step_data: dict):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def get_prev_phase(self) -> int:
-        raise NotImplementedError()
+from agents.agent import Agent
+from roadnet_graph import Intersection
 
 
 class MaxPressureAgent(Agent):
